e366232b60a9f10e665577934381313167dedf53
Paul Santiago <paulsantiago282@gmail.com>
2023-05-21 13:40:30 -0400

    uPdate main

--- main.go
+++ main.go
@@ -2,4 +2,17 @@ package main
 
 func main() {
 
+	weatherData := &WeatherData{
+		Observers:   []Observer{},
+		Temperature: 80.0,
+		Humidity:    65.0,
+		Pressure:    30.0,
+	}
+
+	currentConditions := &CurrentConditions{
+		WeatherData: weatherData,
+	}
+
+	weatherData.RegisterObserver(currentConditions)
+
 }
